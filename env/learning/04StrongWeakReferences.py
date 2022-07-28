4bd2b590feeff615ad59c8c192a1279e002799cf
vivekreddyvari <bobby.vivek@gmail.com>
2022-07-28 09:56:26 +0200

    commit changes-new

--- env/learning/04StrongWeakReferences.py
+++ env/learning/04StrongWeakReferences.py
@@ -1,3 +1,5 @@
+import ctypes
+
 # STrong and WEak Reference
 
 # Strong references
@@ -25,4 +27,69 @@ There is another type of reference in Python -> Weak Reference
     p2 = p1, here p2 is weak reference as it first referred to p1 and later to object
     
     -> del p1 -> No More (strong) reference to object
+              -> Object is garbage collected
+              -> p2 is dead
+    -> So for our data descriptor instead of storing the object as key
+        -> store a weak reference to the object
+    
+    In Python we have module = weakref module
+    
+    p1 = Person() -> P1 has strong reference to the object
+    p2 = weakref.ref(p1)     -> P2 is an (other) object that contains a weak reference
+    
+
+    -> p2 is a callable
+    p2() -> Returns the original object
+         -> or None if the object has been garbage collected
+         
+    careful p3=p2()
+         -> You just created a strong reference to the object
+         
+    
+Dictionaries of Weak Reference:
+so we'll create  a dictionary of weak references( for our keys)
+    -> weakref has a WeakKeyDictionary to do just that!
+    
+    p1 = Person() - > strong reference to the Person instance
+    d = WeakKeyDictionary()
+    d[p2] = 'some value' -> a weak reference is used for the Person instance
+    
+    del p2  -> no more strong references -> garbage collected
+    
+        -> item is automatically removed from weak key dictionary
+        (so be carefully if you're iterating over the dictionary views if that happens
+        during the iteration!
+         we won't need to, so won't be a problem for our use-case)
+         
+    
 """
+
+
+# Coding
+# Counter to the references.
+def ref_count(address):
+    return ctypes.c_long.from_address(address).value
+
+
+class CodeOnePerson:
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return f'Person(name={self.name})'
+
+p1 = CodeOnePerson('Guido')
+p2 = p1
+
+print(f"is p1 a p2:{p1 is p2}, yes id(p1) {id(p1)}= id(p2) {id(p2)} ")
+print(ref_count(id(p1)))
+
+del p2
+print(ref_count(id(p1)))
+p1_id = id(p1)
+print(ref_count(p1_id))
+
+del p1
+print(p1_id)
+
---
+++ env/learning/method.py
@@ -0,0 +1,80 @@
+
+class Classy:
+    def method(self, par):
+        print("Method:", par)
+
+obj = Classy()
+
+for i in range(1, 5):
+    obj.method(i)
+
+
+class TestClassy1:
+    varia = 2
+
+    def method(self):
+        print(self.varia, self.var)
+
+
+test_obj_1 = TestClassy1()
+test_obj_1.var = 3
+test_obj_1.method()
+
+
+# Self parameter is also used to invoke other object/class method from inside the class
+class TestClassy2:
+    def other(self):
+        print('other')
+
+    def method(self):
+        print("method", end="\n\n")
+        self.other()
+
+test_obj_2 = TestClassy2()
+test_obj_2.method()
+
+
+# Inner life of classes and object
+class TestClassy3:
+
+    varia = 1
+    def __init__(self):
+        self.var = 2
+
+    def method(self):
+        pass
+
+    def __hidden(self):
+        pass
+
+test_obj_3 = TestClassy3()
+print(test_obj_3.__dict__, end='\n\n')
+print(TestClassy3.__dict__, end='\n\n')
+
+# Learning
+class Snake:
+    def __init__(self, victims):
+        self.victims = victims
+
+    def increment(self):
+        self.__victims += 1
+
+
+class Emoji:
+    def __init__(self, emoji):
+        self.emoji = emoji
+
+    def dog(self):
+        self.emoji = "\N{dog}"
+        return self.emoji
+
+    def horse(self):
+        self.emoji = "\N{horse}"
+        return self.emoji
+
+d = Emoji('dog')
+print(d.dog())
+h = Emoji('horse')
+print(h.horse())
+
+
