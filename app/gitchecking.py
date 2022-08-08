e58df198821183d9a1ffe224f3f590440ba2e8bb
vivekreddyvari <47451566+vivekreddyvari@users.noreply.github.com>
2022-08-08 10:43:00 +0200

    committing gitchecking.py

---
+++ app/gitchecking.py
@@ -0,0 +1,13 @@
+"""
+This is a sample checkout from PYCHARM to understand, whether the repository and project folders
+are reflecting in GITHUB-> FashionCloud Branch.
+Environments created
+    1. Create Virtual Environment
+        NewLearning/FashionCloud
+            python3 -m venv application
+        source application/bin/activate
+    2. Checkout to the GIT
+        VCS -> Create new branch Cloud
+        commit->push.
+
+"""
