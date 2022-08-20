f614a203f35d9e239e80bbabe923c4ed5e383a46
vivekreddyvari <47451566+vivekreddyvari@users.noreply.github.com>
2022-08-21 00:19:46 +0200

    10/Aug/2022: OOP - Enumerations - Project - completed

---
+++ env/learning/ExceptionSingleInheritance/03handlingexceptions.py
@@ -0,0 +1,94 @@
+
+
+""" 
+Handling Exceptions using try:
+
+try:
+    code that we want to protect from some potential exceptions.
+ keep this guarded code as short as possible
+ often just a single statment
+except:
+    code that will run if that specified <ExceptionType> occurs (or any subclass)
+
+
+    
+finally:
+    code that always executes - whether exception occured or not
+    
+else: 
+    coee that executes if try terminates normally
+    (an except clause must be present)
+    
+
+"""
+try:
+    import sys
+except ImportError as ex:
+    print(ex)
+    # Order matter
+finally:
+    # appears 0 or 1 times
+    print('success')
+    print(f" This is python software {sys.version} version")
+    print(f" This is the OS-VERSION {sys.platform}")
+    for num, mod in enumerate(dir(sys)):
+        print(f"Number {num} = Module `{mod}`")
+    print(f"Version Information {sys.version_info}")
+
+# else:
+    # print('something went wrong')
+    # appears 0 or 1 times # only allowed if an except clause is present
+
+
+lis = [ 1, 2, 3]
+try:
+    lis[3]
+except IndexError:
+    print(f'{IndexError} - invalid Index')
+except LookupError:
+    print(f"{LookupError} - Lookup Error")
+
+
+## Try and finally - mostly used in scenario
+# like open the file in try block and close the file in finally block
+
+try:
+    with open('example.txt' 'r') as file:
+        data = file.read()
+except FileNotFoundError:
+    print('File Not Found')
+# finally:
+    # file.close()
+
+# if exception handler raises an exception>
+"""
+Everything works as normal - If the exception is not handled, it is propageted up.
+EAFP in python
+Easier to ask for Permission
+
+# pythonic way:
+try:
+    with open('file_name', 'r') as f:
+except OSerror:
+    print('error')
+finally:
+    f.close()
+
+other way:
+if os.path.exists(fname) and not os.path.isdir(fname):
+    f = open(fname, 'r')
+    f.close()
+    
+
+"""
+
+from os import path
+if path.exists('example.txt') and not path.isdir('example.txt'):
+    f = open('example.txt', 'r')
+    data_info = f.read()
+    print(data_info)
+    f.close()
+
+# Coding
+
+
